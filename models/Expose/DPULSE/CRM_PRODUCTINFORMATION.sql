dccd0e5411877be4887b46167eb9bd35e67f91b7
ahomo-urw <alban.homo@ext.urw.com>
2025-08-27 15:36:02 +0200

    format data

--- models/Expose/DPULSE/CRM_PRODUCTINFORMATION.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT
-  IFNULL(id, '') AS product_information_id,
-  IFNULL(profitcentersapidname, '') AS profit_center_sap,
-  *
-EXCLUDE(id, profitcentersapidname)
-FROM {{ source('finops', 'CRM_PRODUCTINFORMATION') }}
